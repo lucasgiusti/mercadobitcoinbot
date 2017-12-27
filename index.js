fe7d34ac368769a7ee93e349840b453574c4ac9a
Lucas Giusti <giusti.lucas@gmail.com>
2017-12-27 12:49:35 -0200

    incluindo timer para ordem de venda

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function verifyNegociation(price){
                          console.log('Ordem de compra inserida no livro. ' + JSON.stringify(data))
                          //operando em STOP
                          setTimeout(() =>                          
-                         tradeApi.placeSellOrder(data.order.quantity, parseFloat(tick.ticker.sell * parseFloat(process.env.PROFITABILITY)).toFixed(5), 
+                         tradeApi.placeSellOrder(parseFloat(data.order.quantity).toFixed(8), parseFloat(tick.ticker.sell * parseFloat(process.env.PROFITABILITY)).toFixed(5), 
                              (data) => console.log('Ordem de venda inserida no livro. ' + JSON.stringify(data)),
                              (data) => console.log('Erro ao inserir ordem de venda no livro. ' + data))
                              ,
